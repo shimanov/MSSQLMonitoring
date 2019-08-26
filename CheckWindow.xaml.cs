07cd96c76475b833b2e6ef098f30cb5a856fe5fa
Шиманов Дмитрий Анатольевич <d.a.shimanov@gmail.com>
2019-08-26 16:12:52 +0700

    Update CheckWindow.xaml.cs

--- CheckWindow.xaml.cs
+++ CheckWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Data.SqlClient;
 using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace DatabaseMaintenance
@@ -15,7 +17,8 @@ namespace DatabaseMaintenance
             InitializeComponent();
         }
 
-        readonly string connectionString = File.ReadAllText(Directory.GetCurrentDirectory() + "/DatabaseMaintenance");
+        //readonly string connectionString = File.ReadAllText(Directory.GetCurrentDirectory() + "/DatabaseMaintenance");
+        readonly string connectionString = "Data Source=R54-630099-S;Initial Catalog =DB630099; Integrated Security = SSPI;";
 
         private void StartBtn_Click(object sender, RoutedEventArgs e)
         {
@@ -37,8 +40,27 @@ namespace DatabaseMaintenance
                 command.CommandTimeout = 2147483647;
                 int count = command.ExecuteNonQuery();
                 connection.Close();
-
             }
         }
+
+        //private async Task CheckDb()
+        //{
+        //    string query = "DBCC CHECKDB();";
+
+        //    using (SqlConnection connection = new SqlConnection(connectionString))
+        //    {
+        //        await connection.OpenAsync();
+
+        //        connection.InfoMessage += delegate (object sender, SqlInfoMessageEventArgs e)
+        //        {
+        //            resultTxb.Content += "\n" + e.Message;
+        //        };
+        //        connection.FireInfoMessageEventOnUserErrors = true;
+        //        SqlCommand command = new SqlCommand(query, connection);
+        //        command.CommandTimeout = 2147483647;
+        //        int count = command.ExecuteNonQuery();
+        //        connection.Close();
+        //    }
+        //}
     }
 }
