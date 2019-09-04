511d4fdbe095f90d23e16220e9c9a1f12556ef82
Шиманов Дмитрий Анатольевич <d.a.shimanov@gmail.com>
2019-09-04 16:04:28 +0700

    fix

--- CheckWindow.xaml.cs
+++ CheckWindow.xaml.cs
@@ -4,7 +4,6 @@ using System.ComponentModel;
 using System.Data.SqlClient;
 using System.IO;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace DatabaseMaintenance
@@ -17,7 +16,7 @@ namespace DatabaseMaintenance
         //readonly string connectionString = File.ReadAllText(Directory.GetCurrentDirectory() + "/DatabaseMaintenance");
         readonly string connectionString = "Data Source=10.0.75.1;Initial Catalog =DB633541; User id=sa; password=qwep[]ghjB1";
 
-        private BackgroundWorker backgroundWorker;
+        private readonly BackgroundWorker backgroundWorker;
 
         public CheckWindow()
         {
@@ -33,17 +32,13 @@ namespace DatabaseMaintenance
             backgroundWorker.RunWorkerAsync();
         }
 
-        private void SaveBtn_Click(object sender, RoutedEventArgs e)
-        {
-            SaveFileDialog saveFileDialog = new SaveFileDialog();
-            if (saveFileDialog.ShowDialog() == true)
-            {
-                File.WriteAllText(saveFileDialog.FileName, resultTxb.Content.ToString());
-            }
-        }
+        
 
+        //Вызывается при старте потока, длительная операция
         private void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
         {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
             string query = "DBCC CHECKDB() WITH NO_INFOMSGS, ALL_ERRORMSGS, DATA_PURITY;";
 
             using (SqlConnection connection = new SqlConnection(connectionString))
@@ -63,12 +58,17 @@ namespace DatabaseMaintenance
                 command.CommandTimeout = 2147483647;
                 int count = command.ExecuteNonQuery();
                 connection.Close();
+
+                Thread.Sleep(500);
+                //worker.ReportProgress();
             }
         }
 
+
+        //Отслеживания прогресса выполнения
         private void BackgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
         {
-            string query = "";
+            string query = "use master SELECT [command] ,[start_time] ,[percent_complete] ,[estimated_completion_time] / 60000. AS [estimated_completion_time_min] FROM sys.dm_exec_requests WHERE [command] LIKE '%DBCC%'";
             using (SqlConnection sqlConnection = new SqlConnection(connectionString))
             {
                 try
@@ -108,6 +108,7 @@ namespace DatabaseMaintenance
             }
         }
 
+        //Вызывается по завершению потока
         private void BackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
             if (e.Error != null)
@@ -115,6 +116,16 @@ namespace DatabaseMaintenance
                 ProgressBtn.Visibility = Visibility.Hidden;
                 SaveBtn.Visibility = Visibility.Visible;
                 resultTxb.Content = File.ReadAllText(Directory.GetCurrentDirectory() + "/CheckDb.rpt");
+            }
+        }
+
+        //Сохранение
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, resultTxb.Content.ToString());
             }
         }
     }
