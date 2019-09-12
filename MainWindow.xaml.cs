b364bcdcfe175921e9e31a6ec1a363c196f51721
Шиманов Дмитрий Анатольевич <d.a.shimanov@gmail.com>
2019-09-12 14:14:14 +0700

    Update

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,10 +18,8 @@ namespace DatabaseMaintenance
         {
             InitializeComponent();
 
-            string connectionString = fileStorage.Read("Auth");
-
-            databaseData(connectionString);
-            versionServer(connectionString);
+            databaseData();
+            versionServer();
             memoryServer();
             sqlMemory();
         }
@@ -40,38 +38,14 @@ namespace DatabaseMaintenance
             physMemoryCard.Content = "Общий объем ОЗУ: " + res + " MB";
         }
 
-        private void databaseData(string conn)
+        private void databaseData()
         {
-            string query = "use master select name as [Имя БД] ,state_desc as [Статус]  ,CAST(size / 128.0 as decimal(17,2)) as [Размер MB] from sys.master_files";
-            using (SqlConnection sqlConnection = new SqlConnection(conn))
-            {
-                SqlCommand sqlCommand = new SqlCommand(query, sqlConnection);
-                sqlConnection.Open();
-                using (SqlDataAdapter adapter = new SqlDataAdapter(sqlCommand))
-                {
-                    DataTable dataTable = new DataTable();
-                    adapter.Fill(dataTable);
-                    dataGrid.ItemsSource = dataTable.DefaultView;
-                }
-                sqlConnection.Close();
-            }
+            dataGrid.ItemsSource = script.ExecuteScriptTable("DataBases.sql");
         }
 
-        private void versionServer(string conn)
+        private void versionServer()
         {
-            string query = "use master SELECT SERVERPROPERTY('ServerName') AS [Сервер], SERVERPROPERTY('ProductVersion') AS [Версия продукта], SERVERPROPERTY('Edition') AS [Редакция], SERVERPROPERTY('ProductLevel') AS [SP]; ";
-            using (SqlConnection connection = new SqlConnection(conn))
-            {
-                SqlCommand command = new SqlCommand(query, connection);
-                connection.Open();
-                using (SqlDataAdapter adapter = new SqlDataAdapter(command))
-                {
-                    DataTable dataTable = new DataTable();
-                    adapter.Fill(dataTable);
-                    versionGrid.ItemsSource = dataTable.DefaultView;
-                }
-                connection.Close();
-            }
+            versionGrid.ItemsSource = script.ExecuteScriptTable("InfoMssql.sql");
         }
 
         private void EditConnBtn_Click(object sender, RoutedEventArgs e)
@@ -79,7 +53,6 @@ namespace DatabaseMaintenance
             LoginWindow loginWindow = new LoginWindow();
             loginWindow.Show();
             Close();
-            memoryCard.Content = "Объем использованной ОЗУ SQL:";
         }
 
         private void CheckBtn_Click(object sender, RoutedEventArgs e)
